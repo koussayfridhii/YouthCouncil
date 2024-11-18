f3b25399ff53aa4a587e4ef1273bd5b46df264d1
koussayfridhii <koussayfridhi.kf@gmail.com>
2024-11-18 18:01:57 +0100

    trying to optimise the loading time

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import "./App.css";
+import { lazy, useEffect, useState } from "react";
 import HeroScene from "./components/scenes/HeroScene";
-import TopicsScene from "./components/scenes/TopicsScene";
-
+import { useProgress } from "@react-three/drei";
+const TopicsScene = lazy(() => import("./components/scenes/TopicsScene"));
 function App() {
+  const { progress } = useProgress();
+  const [isHeroLoaded, setIsHeroLoaded] = useState(false);
+  useEffect(() => {
+    progress == 100 && setIsHeroLoaded(true);
+  }, [progress]);
   return (
     <>
-      <HeroScene />
-      <TopicsScene />
+      <HeroScene onLoaded={() => console.log(true)} />
+      {isHeroLoaded && <TopicsScene />}
     </>
   );
 }
