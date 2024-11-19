4c7fdab7fb948ed8d6cfef3df66bc7ba12687c55
koussayfridhii <koussayfridhi.kf@gmail.com>
2024-11-19 13:25:15 +0100

    minor fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
 import "./App.css";
-import { lazy, useEffect, useState } from "react";
+import { lazy } from "react";
 import HeroScene from "./components/scenes/HeroScene";
-import { useProgress } from "@react-three/drei";
 const TopicsScene = lazy(() => import("./components/scenes/TopicsScene"));
 function App() {
-  const { progress } = useProgress();
-  const [isHeroLoaded, setIsHeroLoaded] = useState(false);
-  useEffect(() => {
-    progress == 100 && setIsHeroLoaded(true);
-  }, [progress]);
   return (
     <>
-      <HeroScene onLoaded={() => console.log(true)} />
-      {isHeroLoaded && <TopicsScene />}
+      <HeroScene />
+      <TopicsScene />
+      {/* {isHeroLoaded ? <TopicsScene /> : <div className="h-dvh w-full"></div>} */}
     </>
   );
 }
